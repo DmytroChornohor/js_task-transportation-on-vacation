667e722b70e1410fb5bda0879c42918720705a7a
Dmytro Chornohor <chornohor@Mac-mini.local>
2025-11-15 18:57:00 +0200

    Add rental cost calculator(without numbers)

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const dailyRentalRate = 40;
+  const shortTermDiscount = 20;
+  const longTermDiscount = 50;
 
   if (days < 3){
-    return days * 40;
+    return days * dailyRentalRate;
   }
 
   if (days >= 3 && days < 7){
-    return (days * 40) - 20;
+    return (days * dailyRentalRate) - shortTermDiscount;
   }
 
   if (days >= 7){
-    return (days * 40) - 50;
+    return (days * dailyRentalRate) - longTermDiscount;
   }
 }
 
